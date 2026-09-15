4ed5bdea2c1884267c919044646e80740773ed24
Al Smaznov <torovfritz@ya.ru>
2026-09-15 23:41:40 +0300

    Исправлена ошибка автотестов 1

--- src/main.js
+++ src/main.js
@@ -41,7 +41,15 @@ function calculateBonusByProfit(index, total, seller) {
  */
 function analyzeSalesData(data, options) {
   // @TODO: Проверка входных данных
-  if (!data || !Array.isArray(data.sellers) || data.sellers.length === 0) {
+  if (
+    !data ||
+    !Array.isArray(data.sellers) ||
+    data.sellers.length === 0 ||
+    !Array.isArray(data.products) ||
+    data.products.length === 0 ||
+    !Array.isArray(data.purchase_records) ||
+    data.purchase_records.length === 0
+  ) {
     throw new Error("Некорректные входные данные");
   }
 
